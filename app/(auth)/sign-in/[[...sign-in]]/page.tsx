d270de5e06c5c7ecc83c216534b5299a9eb6d488
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-21 11:51:35 +0530

    sign in issues

--- app/(auth)/sign-in/[[...sign-in]]/page.tsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page(){
-    return(
-        <div className="flex items-center h-screen justify-center">
-            <SignIn/>
-        </div>
-    )
-}
