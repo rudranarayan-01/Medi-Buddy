5d909bb87120d21fd8a45031c3db96170f3d621e
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-21 20:18:08 +0530

    db setup drizzle neon

---
+++ app/api/users/route.tsx
@@ -0,0 +1,31 @@
+import { db } from "@/config/db";
+import { usersTable } from "@/config/schema";
+import { currentUser } from "@clerk/nextjs/server";
+import { eq } from "drizzle-orm";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    const user = await currentUser()
+
+    try {
+        // Check if user is already exists
+        const users = await db.select().from(usersTable)
+            //@ts-ignore
+            .where(eq(usersTable.email, user?.primaryEmailAddress?.emailAddress))
+        if (users?.length == 0) {
+            const result = await db.insert(usersTable).values({
+                //@ts-ignore
+                name: user?.fullName,
+                email: user?.primaryEmailAddress?.emailAddress,
+                credits: 10
+            }).returning({ usersTable })
+            return NextResponse.json(result);
+
+        }
+        return NextResponse.json(users[0]);
+        // If not then create new user
+    } catch (error) {
+        return NextResponse.json(error);
+    }
+
+}
