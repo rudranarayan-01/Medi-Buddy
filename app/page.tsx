3f958c37529d4f01bfcfd39c8520a49c2cb909a2
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-21 12:38:22 +0530

    navbar setup

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,9 @@
 
 import { motion } from "motion/react";
 import { FeatureBentoGrid } from "./_components/FeatureBentoGrid";
+import { UserButton, useUser } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -50,8 +53,8 @@ export default function Home() {
           }}
           className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
         >
-         Deliver instant, accurate medical assistance through natural voice conversations.
-         Automate appointment scheduling, symptom triage, and follow-up care-24x7
+          Deliver instant, accurate medical assistance through natural voice conversations.
+          Automate appointment scheduling, symptom triage, and follow-up care-24x7
         </motion.p>
         <motion.div
           initial={{
@@ -99,21 +102,28 @@ export default function Home() {
           </div>
         </motion.div>
       </div>
-      <FeatureBentoGrid/>
+      <FeatureBentoGrid />
     </div>
   );
 }
 
 const Navbar = () => {
+  const { user } = useUser()
+
   return (
     <nav className="flex w-full items-center justify-between border-t border-b border-neutral-200 px-4 py-4 dark:border-neutral-800">
       <div className="flex items-center gap-2">
         <div className="size-7 rounded-full bg-gradient-to-br from-violet-500 to-pink-500" />
         <h1 className="text-base font-bold md:text-2xl">Aceternity UI</h1>
       </div>
-      <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+      {!user ? <Link href={"/sign-in"}> <button className="w-24 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 md:w-32 dark:bg-white dark:text-black dark:hover:bg-gray-200">
         Login
-      </button>
+      </button> </Link> :
+        <div className="flex gap-5 items-center">
+          <Button>Dashboard</Button>
+          <UserButton />
+        </div>
+      }
     </nav>
   );
 };
