829ddfc591808d37ab49056f2611a3063442c30a
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-21 12:46:28 +0530

    hero section done

--- app/page.tsx
+++ app/page.tsx
@@ -56,6 +56,8 @@ export default function Home() {
           Deliver instant, accurate medical assistance through natural voice conversations.
           Automate appointment scheduling, symptom triage, and follow-up care-24x7
         </motion.p>
+
+        <Link href="/sign-in">
         <motion.div
           initial={{
             opacity: 0,
@@ -70,12 +72,11 @@ export default function Home() {
           className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
         >
           <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-            Explore Now
+            Get Started
           </button>
-          <button className="w-60 transform rounded-lg border border-gray-300 bg-white px-6 py-2 font-medium text-black transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-100 dark:border-gray-700 dark:bg-black dark:text-white dark:hover:bg-gray-900">
-            Contact Support
-          </button>
+         
         </motion.div>
+        </Link>
         <motion.div
           initial={{
             opacity: 0,
