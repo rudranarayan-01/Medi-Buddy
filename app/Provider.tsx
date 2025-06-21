b96ef8c59ae616d26b67fa5b7bf00b8a46b07f93
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-21 20:30:08 +0530

    user setup

--- app/Provider.tsx
+++ app/Provider.tsx
@@ -1,26 +1,40 @@
 "use client";
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useUser } from "@clerk/nextjs";
+import { UserDetailContext } from "@/context/UserDetailContext";
+
+export type UsersDetail = {
+  name: string,
+  email: string,
+  credits: number,
+}
 
 function Provider({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
-  
-  const {user} = useUser();
 
-  useEffect(()=>{
+  const { user } = useUser();
+
+  const [userDetail, setUserDetail] = useState<any>
+
+  useEffect(() => {
     user && CreateNewUser();
-  },[user])
+  }, [user])
 
-  const CreateNewUser = async() =>{
+  const CreateNewUser = async () => {
     const result = await axios.post('/api/users');
     console.log(result.data);
   }
-  return <div>{children}</div>;
+  return <div>
+    <UserDetailContext.Provider value={{userDetail, setUserDetail}}>
+
+      {children}
+    </UserDetailContext.Provider>
+  </div>;
 }
 
 export default Provider;
---
+++ context/UserDetailContext.tsx
@@ -0,0 +1,4 @@
+import { UsersDetail } from "@/app/Provider";
+import { createContext } from "react";
+
+export const UserDetailContext = createContext<UsersDetail | undefined >(undefined)
