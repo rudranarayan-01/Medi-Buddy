cf01ffde34e40b429e01120f6230f907a411cefd
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-22 08:25:31 +0530

    dasboard setup

---
+++ app/(routes)/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import AppHeader from './_components/AppHeader';
+
+function DashboardLayout({
+    children,
+}: Readonly<{
+    children: React.ReactNode;
+}>) {
+    return (
+        <div>
+            <AppHeader/>
+            {children}
+        </div>
+    )
+}
+
+export default DashboardLayout
