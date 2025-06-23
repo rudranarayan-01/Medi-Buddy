2ded14365cdb6975c4a49f1178bfb0dc3f02a652
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-23 07:54:33 +0530

    dialog completed

--- app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
+++ app/(routes)/dashboard/_components/AddNewSessionDialog.tsx
@@ -1,4 +1,5 @@
-import React from 'react'
+"use client";
+import React, { useState } from 'react'
 
 import {
   Dialog,
@@ -12,8 +13,10 @@ import {
 } from "@/components/ui/dialog"
 import { Button } from '@/components/ui/button'
 import { Textarea } from '@/components/ui/textarea'
+import { ArrowRightIcon } from 'lucide-react'
 
 function AddNewSessionDialog() {
+  const [note, setNote] = useState<string>();
   return (
     <Dialog>
       <DialogTrigger>
@@ -25,7 +28,7 @@ function AddNewSessionDialog() {
           <DialogDescription asChild>
             <div>
               <h2>Add Symptoms and Any Other Details</h2>
-              <Textarea placeholder='Add details here' className='h-[150px] mt-1' />
+              <Textarea placeholder='Add details here' className='h-[150px] mt-1' onChange={(e)=>setNote(e.target.value)} />
             </div>
           </DialogDescription>
         </DialogHeader>
@@ -33,7 +36,7 @@ function AddNewSessionDialog() {
           <DialogClose>
             <Button variant="destructive">Cancel</Button>
           </DialogClose>
-          <Button>Start</Button>
+          <Button disabled={!note} >Next<ArrowRightIcon/></Button>
         </DialogFooter>
       </DialogContent>
     </Dialog>
