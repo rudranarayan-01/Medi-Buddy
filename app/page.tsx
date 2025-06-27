2ace4c5e7e8d1f375d4fbecb51d1b04d9777a248
Rudranarayan Sahu <rudranarayansahu080@gmail.com>
2025-06-27 11:09:58 +0530

    home pages changes

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ import { UserButton, useUser } from "@clerk/nextjs";
 import { Button } from "@/components/ui/button";
 import Link from "next/link";
 import Image from "next/image";
+import { BackgroundLines } from "@/components/ui/background-lines";
 
 export default function Home() {
   return (
@@ -21,45 +22,29 @@ export default function Home() {
       <div className="absolute inset-x-0 bottom-0 h-px w-full bg-neutral-200/80 dark:bg-neutral-800/80">
         <div className="absolute mx-auto h-px w-40 bg-gradient-to-r from-transparent via-blue-500 to-transparent" />
       </div>
-      <div className="px-4 py-10 md:py-20">
-        <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
-          {"Revolutionize Patient Care with AI Voice Agents"
-            .split(" ")
-            .map((word, index) => (
-              <motion.span
-                key={index}
-                initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
-                animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
-                transition={{
-                  duration: 0.3,
-                  delay: index * 0.1,
-                  ease: "easeInOut",
-                }}
-                className="mr-2 inline-block"
-              >
-                {word}
-              </motion.span>
-            ))}
-        </h1>
-        <motion.p
-          initial={{
-            opacity: 0,
-          }}
-          animate={{
-            opacity: 1,
-          }}
-          transition={{
-            duration: 0.3,
-            delay: 0.8,
-          }}
-          className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
-        >
-          Deliver instant, accurate medical assistance through natural voice conversations.
-          Automate appointment scheduling, symptom triage, and follow-up care-24x7
-        </motion.p>
 
-        <Link href="/sign-in">
-          <motion.div
+      <BackgroundLines >
+        <div className="px-4 py-10 md:py-20">
+          <h1 className="relative z-10 mx-auto max-w-4xl text-center text-2xl font-bold text-slate-700 md:text-4xl lg:text-7xl dark:text-slate-300">
+            {"Revolutionize Patient Care with AI Voice Agents"
+              .split(" ")
+              .map((word, index) => (
+                <motion.span
+                  key={index}
+                  initial={{ opacity: 0, filter: "blur(4px)", y: 10 }}
+                  animate={{ opacity: 1, filter: "blur(0px)", y: 0 }}
+                  transition={{
+                    duration: 0.3,
+                    delay: index * 0.1,
+                    ease: "easeInOut",
+                  }}
+                  className="mr-2 inline-block"
+                >
+                  {word}
+                </motion.span>
+              ))}
+          </h1>
+          <motion.p
             initial={{
               opacity: 0,
             }}
@@ -68,45 +53,66 @@ export default function Home() {
             }}
             transition={{
               duration: 0.3,
-              delay: 1,
+              delay: 0.8,
             }}
-            className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+            className="relative z-10 mx-auto max-w-xl py-4 text-center text-lg font-normal text-neutral-600 dark:text-neutral-400"
           >
-            <Link href={"/dashboard"}>
-              <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
-                Get Started
-              </button>
-            </Link>
+            Deliver instant, accurate medical assistance through natural voice conversations.
+            Automate appointment scheduling, symptom triage, and follow-up care-24x7
+          </motion.p>
 
+          <Link href="/sign-in">
+            <motion.div
+              initial={{
+                opacity: 0,
+              }}
+              animate={{
+                opacity: 1,
+              }}
+              transition={{
+                duration: 0.3,
+                delay: 1,
+              }}
+              className="relative z-10 mt-8 flex flex-wrap items-center justify-center gap-4"
+            >
+              <Link href={"/dashboard"}>
+                <button className="w-60 transform rounded-lg bg-black px-6 py-2 font-medium text-white transition-all duration-300 hover:-translate-y-0.5 hover:bg-gray-800 dark:bg-white dark:text-black dark:hover:bg-gray-200">
+                  Get Started
+                </button>
+              </Link>
+
+            </motion.div>
+          </Link>
+          <motion.div
+            initial={{
+              opacity: 0,
+              y: 10,
+            }}
+            animate={{
+              opacity: 1,
+              y: 0,
+            }}
+            transition={{
+              duration: 0.3,
+              delay: 1.2,
+            }}
+            className="relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
+          >
+            <div className="w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700">
+              <img
+                src="https://assets.aceternity.com/pro/aceternity-landing.webp"
+                alt="Landing page preview"
+                className="aspect-[16/9] h-auto w-full object-cover"
+                height={1000}
+                width={1000}
+              />
+            </div>
           </motion.div>
-        </Link>
-        <motion.div 
-          initial={{
-            opacity: 0,
-            y: 10,
-          }}
-          animate={{
-            opacity: 1,
-            y: 0,
-          }}
-          transition={{
-            duration: 0.3,
-            delay: 1.2,
-          }}
-          className="relative z-10 mt-20 rounded-3xl border border-neutral-200 bg-neutral-100 p-4 shadow-md dark:border-neutral-800 dark:bg-neutral-900"
-        >
-          <div className="w-full overflow-hidden rounded-xl border border-gray-300 dark:border-gray-700">
-            <img
-              src="https://assets.aceternity.com/pro/aceternity-landing.webp"
-              alt="Landing page preview"
-              className="aspect-[16/9] h-auto w-full object-cover"
-              height={1000}
-              width={1000}
-            />
-          </div>
-        </motion.div>
-      </div>
+        </div>
+     
+
       <FeatureBentoGrid />
+       </BackgroundLines>
     </div>
   );
 }
